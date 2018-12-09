aba37adf4217872c22d74bee0cb8a56a7805d622
Erik Wotring <ewotring@gmail.com>
2018-12-09 12:51:16 -0600

    Show rudimentary list of players

--- src/components/list.js
+++ src/components/list.js
@@ -2,7 +2,36 @@ import React, { Component } from 'react'
 
 class List extends Component {
   render() {
-    return <div>lists, lists, lists!</div>
+    console.log(this.props.items);
+    console.log(this.props.items.roster);
+    let players = this.props.items.roster;
+
+    // return <div>lists, lists, lists!</div>
+    // create async image component and use here
+    // use separate component for content?
+    // Nope, the data for the image is a URL, so I can just do another fetch inside
+    // of the list component.
+    // Should I count on the image then loading into cache and passing that data to the
+    // detail view, or should I do another fetch in the detail view?
+    return (
+      <ul>
+        {players.map(player => (
+          <li
+            key = {player.name}
+          >
+            <div>
+              Player Name: {player.name}
+            </div>
+            <div>
+              Player Image: {player.image_url}
+            </div>
+            <div>
+              Player Position: {player.position}
+            </div>
+          </li>
+        ))}
+      </ul>
+    )
   }
 }
 

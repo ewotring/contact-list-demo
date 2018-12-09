d12bbc62d90cd69707f961c75f9f0a6ff62391a2
Erik Wotring <ewotring@gmail.com>
2018-12-08 23:29:51 -0600

    WIP build app and start on list component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
+import List from './components/list'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://assets.gpshopper.com/jc/android_eval.json")
+      .then(result => result.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
   render() {
-    return (
-      <div className="App">
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return (
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>Data loading...</div>
         </header>
-      </div>
-    );
+      )
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <div>Here's the list!</div>
+          </header>
+        <List items = { items } />
+        </div>
+      );
+    }
   }
 }
 
---
+++ src/components/list.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+  render() {
+    return <div>lists, lists, lists!</div>
+  }
+}
+
+export default List
